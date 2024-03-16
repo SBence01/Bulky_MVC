657cc415f9bba517cd5629e318e0e064f5f63806
Bence Sárga <sargabence26@gmail.com>
2024-03-16 15:18:59 +0100

    .

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -28,10 +28,10 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-    app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"));
+    
 }
-
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"));
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
